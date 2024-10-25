d86596598b3cbe2afd22f2180b5f1d53796344e9
Shirubae <pequignotfrancois@gmail.com>
2024-10-25 17:36:12 +0200

    add panelspage.vue

---
+++ src/views/Panels/PanelsPage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'PanelsPage',
+}
+</script>
+
+<template>
+  <div class="text-center">
+    <p class="text-2xl">Page Panneaux</p>
+  </div>
+</template>
+
+<style></style>
