254b1b18e18e058e257eb831589b315fc54cd091
Shirubae <pequignotfrancois@gmail.com>
2024-10-29 17:04:33 +0100

    update home-page: WIP

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,9 +5,52 @@ export default {
 </script>
 
 <template>
-  <div class="text-center">
-    <p class="text-2xl">Page d'accueil</p>
+  <div class="flex flex-col min-h-screen">
+    <div class="flex justify-center mt-8">
+      <p class="text-2xl text-center">Page d'accueil</p>
+    </div>
+
+    <div class="flex flex-1 items-center justify-end pr-8">
+      <div class="carousel w-[500px] h-[400px]">
+        <div id="slide1" class="carousel-item relative w-full h-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+            class="w-full h-full object-cover" />
+          <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide4" class="btn btn-circle">❮</a>
+            <a href="#slide2" class="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide2" class="carousel-item relative w-full h-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+            class="w-full h-full object-cover" />
+          <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide1" class="btn btn-circle">❮</a>
+            <a href="#slide3" class="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide3" class="carousel-item relative w-full h-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+            class="w-full h-full object-cover" />
+          <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide2" class="btn btn-circle">❮</a>
+            <a href="#slide4" class="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide4" class="carousel-item relative w-full h-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+            class="w-full h-full object-cover" />
+          <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide3" class="btn btn-circle">❮</a>
+            <a href="#slide1" class="btn btn-circle">❯</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style></style>
+
