2d428a9a00cf98b5e8c1272ed329ab37ed3ab3c7
mcprodev <mcpro.dev@proton.me>
2024-11-02 12:43:51 +0100

    add interactive_map: map display WIP

--- src/views/InteractiveMap.vue
+++ src/views/InteractiveMap.vue
@@ -1,13 +1,43 @@
-<script>
-export default {
-  name: 'InteractiveMap',
-}
+<script setup>
+import 'leaflet/dist/leaflet.css'
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
+import { ref } from 'vue'
+
+// import arcades from '@/arcade.json'
+
+let zoom = ref(6)
+let center = ref([38, 139.69])
 </script>
 
 <template>
   <div class="text-center">
-    <p class="text-2xl">Map</p>
+    <p class="text-2xl">Carte intéractive</p>
   </div>
+  <main>
+    <l-map
+      ref="map"
+      v-model:zoom="zoom"
+      v-model:center="center"
+      :useGlobalLeaflet="false"
+    >
+      <l-tile-layer
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        layer-type="base"
+        name="Stadia Maps Basemap"
+      ></l-tile-layer>
+    </l-map>
+  </main>
 </template>
 
-<style></style>
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
