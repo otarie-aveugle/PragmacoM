6266399a4afaea8b57ee3550bb12cce4137c6f63
mcprodev <mcpro.dev@proton.me>
2024-11-06 10:55:15 +0100

    update_HomePage: add editableDesign

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,18 @@
 <script>
+import { useUserStore } from '@/stores/user'
+import { mapState } from 'pinia'
+
+import Panneau1 from '../assets/images/Panneau1.jpg'
+import Panneau2 from '../assets/images/Panneau2.jpg'
+import Panneau3 from '../assets/images/Panneau3.jpg'
+import Panneau4 from '../assets/images/Panneau4.jpg'
+
 export default {
   name: 'HomePage',
   data() {
     return {
+      isEditing: false,
+      editableImage: null,
       currentSlide: 0,
       slides: [
         {
@@ -22,9 +32,40 @@ export default {
             'https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp',
         },
       ],
+      faces: [
+        {
+          image: Panneau1,
+        },
+        {
+          image: Panneau2,
+        },
+        {
+          image: Panneau3,
+        },
+        {
+          image: Panneau4,
+        },
+      ],
     }
   },
+  computed: {
+    ...mapState(useUserStore, ['userLoggedIn']),
+  },
   methods: {
+    toggleEdit() {
+      this.isEditing = !this.isEditing
+    },
+    editImage(index, type) {
+      this.editableImage = { index, type }
+    },
+    updateImage(newUrl) {
+      if (this.editableImage.type === 'carousel') {
+        this.slides[this.editableImage.index].image = newUrl
+      } else if (this.editableImage.type === 'faces') {
+        this.faces[this.editableImage.index].image = newUrl
+      }
+      this.editableImage = null
+    },
     nextSlide() {
       this.currentSlide = (this.currentSlide + 1) % this.slides.length
     },
@@ -38,20 +79,31 @@ export default {
 
 <template>
   <div class="flex flex-col mx-4 gap-6">
+    <div v-if="userLoggedIn" class="flex justify-end">
+      <button @click="toggleEdit" class="btn btn-sm btn-outline btn-primary">
+        <span v-if="isEditing">Arrêter l'édition</span>
+        <span v-else>Éditer la page</span>
+      </button>
+    </div>
+
     <div
       class="flex flex-col gap-6 md:flex-row md:justify-between md:items-center"
     >
       <!-- texte+CTA -->
       <div class="flex flex-col gap-y-12 md:w-1/2 items-center md:items-start">
         <h1
+          :contenteditable="isEditing"
           class="text-7xl font-bold text-center sm:text-5xl md:text-6xl md:text-start lg:text-7xl"
+          :class="{ 'border border-dashed border-primary': isEditing }"
         >
           Un panneau, <br />
           <span class="text-primary">1000</span> regards
         </h1>
 
         <p
+          :contenteditable="isEditing"
           class="text-sm text-gray-700 leading-relaxed sm:text-base md:text-lg"
+          :class="{ 'border border-dashed border-primary': isEditing }"
         >
           Donec tincidunt ultricies dui, at tincidunt nisl porta eu. Fusce nec
           ipsum dignissim, egestas nisl a, interdum dui. Fusce urna erat,
@@ -98,7 +150,39 @@ export default {
               flex: currentSlide === index,
             }"
           >
-            <img :src="slide.image" class="w-full h-full object-cover" />
+            <img
+              :src="slide.image"
+              class="w-full h-full object-cover"
+              :class="{ 'border border-dashed border-primary': isEditing }"
+            />
+            <div
+              v-if="
+                isEditing &&
+                editableImage?.index === index &&
+                editableImage?.type === 'carousel'
+              "
+              class="absolute top-5 left-5 bg-white p-2 shadow rounded"
+            >
+              <input
+                type="text"
+                v-model="slides[index].image"
+                @blur="updateImage(slides[index].image)"
+                class="input input-sm"
+                placeholder="Image URL"
+              />
+            </div>
+            <button
+              v-if="isEditing"
+              @click="editImage(index, 'carousel')"
+              class="btn btn-sm absolute bottom-5 left-5"
+              style="
+                background-color: rgba(0, 0, 0, 0.6);
+                color: white;
+                font-weight: bold;
+              "
+            >
+              Modifier
+            </button>
             <div
               class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
             >
@@ -121,42 +205,52 @@ export default {
         class="flex flex-row flex-wrap gap-6 justify-center md:justify-start"
       >
         <figure
-          class="w-96 h-64 flex items-center justify-center bg-gray-200 rounded-t-lg"
+          v-for="(face, index) in faces"
+          :key="index"
+          class="w-96 h-64 flex items-center justify-center bg-gray-200 rounded-t-lg relative"
         >
           <img
-            src="../assets/images/Panneau1.jpg"
+            :src="face.image"
             alt="panel"
             class="w-full h-full object-cover rounded-t-lg"
           />
-        </figure>
-        <figure
-          class="w-96 h-64 flex items-center justify-center bg-gray-200 rounded-t-lg"
-        >
-          <img
-            src="../assets/images/Panneau2.jpg"
-            alt="panel"
-            class="w-full h-full object-cover rounded-t-lg"
-          />
-        </figure>
-        <figure
-          class="w-96 h-64 flex items-center justify-center bg-gray-200 rounded-t-lg"
-        >
-          <img
-            src="../assets/images/Panneau3.jpg"
-            alt="panel"
-            class="w-full h-full object-cover rounded-t-lg"
-          />
-        </figure>
-        <figure
-          class="w-96 h-64 flex items-center justify-center bg-gray-200 rounded-t-lg"
-        >
-          <img
-            src="../assets/images/Panneau4.jpg"
-            alt="panel"
-            class="w-full h-full object-cover rounded-t-lg"
-          />
+          <div
+            v-if="
+              isEditing &&
+              editableImage?.index === index &&
+              editableImage?.type === 'faces'
+            "
+            class="absolute top-5 left-5 bg-white p-2 shadow rounded"
+          >
+            <input
+              type="text"
+              v-model="faces[index].image"
+              @blur="updateImage(faces[index].image)"
+              class="input input-sm"
+              placeholder="Image URL"
+            />
+          </div>
+          <button
+            v-if="isEditing"
+            @click="editImage(index, 'faces')"
+            class="btn btn-sm absolute bottom-5 left-5"
+            style="
+              background-color: rgba(0, 0, 0, 0.6);
+              color: white;
+              font-weight: bold;
+            "
+          >
+            Modifier
+          </button>
         </figure>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+[contenteditable='true'] {
+  outline: 2px dashed #6b7280;
+  padding: 2px;
+}
+</style>
