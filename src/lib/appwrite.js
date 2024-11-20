2359efd504808e804ab079a9ccfcf93152c8404b
mcprodev <mcpro.dev@proton.me>
2024-11-20 14:53:52 +0100

    update ID appwrite to prod account pragmacom appwrite and fix class overflow on panels page

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -4,7 +4,7 @@ export const client = new Client()
 
 client
   .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('6713c15e00218d71e784')
+  .setProject('673c926d00383fb473c2')
 
 export const databases = new Databases(client)
 export const account = new Account(client)
